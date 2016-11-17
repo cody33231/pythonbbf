1f79fe8092983d0df989ecd04ed54d9f981ee82f
cody <cody_chen@163.com>
2016-11-17 11:22:08 +0800

    add ex3.py

--- ex3.py
+++ ex3.py
@@ -1,5 +1,16 @@
-print "I will now count my chickens:"
-print "Hens", 25 + 30 / 6
-print "Roos ters", 100 - 25 * 3 % 4
+#--coding: utf-8 --
+print "I will now count my chickens:" #打印字符串
+print "Hens", float(25 + 30 / 6) #打印 加和除
+print "Roos ters", float(100 - 25 * 3 % 4) #打印 100 减去 
 print "Now I will count the eggs:"
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 输出的只有整数，需要浮点数需加入float
+print float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7 #输出布尔值
+print "What is 3 + 2?", float(3 + 2)
+print "What is 5 - 7?", float(5 - 7)
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2 #输出布尔值
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
