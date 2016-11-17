61c60320eea2a81fd245c4e2da4e08c9cdf8d652
cody33231 <cody_chen@163.com>
2016-11-17 11:53:50 +0800

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,7 +1,7 @@
 # --coding: utf-8 --
 
 cars = 100
-space_in_a_car = 4 #如果这里使用整数4.0，那么变量“carpool_capacity = cars_driven（30） * space_in_a_car（4.0）的值就是120.0
+space_in_a_car = 4 #如果这里使用浮点数4.0，那么变量“carpool_capacity = cars_driven（30） * space_in_a_car（4.0）的值就是120.0
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
