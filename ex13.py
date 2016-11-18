c95a89f3dc9fd5ba76f533759f2b7a95a867b680
cody <cody_chen@163.com>
2016-11-18 10:27:05 +0800

    add ex13.py

---
+++ ex13.py
@@ -0,0 +1,25 @@
+#-- coding=utf-8 --
+
+from sys import argv
+
+script, first, second, third = argv
+
+'''print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third'''
+
+print "The script is called:", raw_input('script:')
+print "Your first variable is:", raw_input('first')
+print "Your second variable is:", raw_input('second')
+print "Your third variable is:", raw_input('third')
+
+
+
+'''aa, bb, cc, dd, ee = argv
+print "The script is called aa:", aa
+print "Your first variable is bb:", bb
+print "Your second variable is cc:", cc
+print "Your third variable is dd:", dd
+print "Your third variable is ee:", ee'''
+
