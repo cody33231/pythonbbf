d3f0a8ed3f7c11d953df76e9f93230b0be672ffa
cc <cody_chen@163.com>
2016-11-19 13:23:40 +0800

    add ex17.py

--- ex17.py
+++ ex17.py
@@ -1,4 +1,25 @@
-# --coding=utf-8 --
+# --coding=utf-8--
+from sys import argv
+from os.path import exists
 
-def fname(arg):
-    pass
+script, from_file, to_file = argv
+
+print "Copying from %s to %s " % (from_file, to_file)
+
+#we could do these two on one line too, how?
+input = open(from_file)
+indate = input.read()
+
+print "The input file is %d bytes long" % len(indate)
+
+print "does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETRUN to continue, CTRL-C to abort."
+raw_input()
+
+output = open(to_file,'w')
+output.write(indate)
+
+print "Alringht, all done"
+
+output.close()
+input.close()
