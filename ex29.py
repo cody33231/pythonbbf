edd4c3bbb620ae5bcc0945a6ac3bc79bcc009b07
cc <cody_chen@163.com>
2016-11-22 21:27:01 +0800

    add ex29.py

---
+++ ex29.py
@@ -0,0 +1,31 @@
+# --coding=utf-8--
+
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats: # 20 < 30
+    print "Too many cats! The world is doomed!"
+
+if people > cats: # 20 > 30
+    print "Not many cats! The world is saved!"
+
+if people < dogs: #20 < 15
+    print "The world is drooled on!"
+
+if people > dogs: # 20 > 15
+    print "The world is dry!"
+
+
+dogs += 5  # 15 +5
+
+if people >= dogs:  #20 >= 20
+    print "People are greater than or equal to dogs."
+
+
+if people <= dogs:  # 20 <= 20
+    print "People are less than or equal to dogs."
+
+if people == dogs: #　２0 == 20
+    print "People are dogs."
